cb0cabdccd64bbf92dc44bdb63d53f3d801d315c
sinergispratama <sinergispratama23@gmail.com>
2024-10-05 20:02:48 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rdtr/';
+$config['base_url'] = 'http://151.106.113.127/rdtr/';
 
 /*
 |--------------------------------------------------------------------------
